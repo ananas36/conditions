bcede5143015d591fd64b0f83e7388c5f5bb2302
serhii <sergijlosnij@gmail.com>
2026-01-31 15:17:43 +0200

    add condition

--- conditions/Program.cs
+++ conditions/Program.cs
@@ -10,6 +10,18 @@ namespace conditions
     {
         static void Main(string[] args)
         {
+            int one = 1; int two=1; int three = 3;
+            int four = 4; int five = 5; int six = 6;
+            int seven = 7; int ten = 10; int eleven = 11;
+            if (one == two)
+            {
+                Console.WriteLine("one=two");
+
+            }
+            else
+            {
+                Console.WriteLine("one!=two");
+            }
         }
     }
 }
