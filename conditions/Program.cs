0cb51ef8b8165b73f1d13e272b2ac728cd1fe9f3
serhii <sergijlosnij@gmail.com>
2026-01-31 15:43:59 +0200

    add loop

--- conditions/Program.cs
+++ conditions/Program.cs
@@ -10,18 +10,26 @@ namespace conditions
     {
         static void Main(string[] args)
         {
-            int one = 1; int two=1; int three = 3;
+            int one = 1; int two=2; int three = 3;
             int four = 4; int five = 5; int six = 6;
             int seven = 7; int ten = 10; int eleven = 11;
+            //==;<=;>=;<;>;!=;
             if (one == two)
             {
                 Console.WriteLine("one=two");
-
             }
             else
             {
                 Console.WriteLine("one!=two");
             }
+
+            int[] digits = { 11, 2, 6, 9, 5,3,2 };
+            Console.WriteLine(digits.Length);
+            for (int i = 0; i < digits.Length; i++)
+            {
+                Console.WriteLine(digits[i]);
+            }
+
         }
     }
 }
